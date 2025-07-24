5a9d7fcca899213db86c2f9668bfd88e0013c3c3
ALIMAGNIDOKPO Arix <127417709+Arix-ALIMAGNIDOKPO@users.noreply.github.com>
2025-07-24 16:45:53 +0100

    update

--- backend/app.py
+++ backend/app.py
@@ -345,4 +345,4 @@ def method_not_allowed(err):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') != 'production'
-    app.run(host='0.0.0.0', port=port, debug=debug, threaded=True)
+    app.run(host='0.0.0.0', port=port, debug=debug, threaded=True) 
